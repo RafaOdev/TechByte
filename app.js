b1585c1ea613fe575800fe6a37e37044b7c496de
Rafael Oliveira <132023935+RafaOdev@users.noreply.github.com>
2024-05-21 11:45:48 -0300

    version-1.0

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = require('./src/routes/routes.js');
-
+const path = require('path');
 
 const app = express();
-app.use(router);
-
-
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, './src/public')));
+app.use(router);
 
 app.listen(PORT, () => {
     console.log('funcionando');
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
-router.get('/news', (req, res) => {
-    res.send('In construction...');
+
+router.get('/', (req, res) => {
+    const homePath = path.join(__dirname, '../public/home/index.html');
+    res.sendFile(homePath);
 })
 
 module.exports = router
